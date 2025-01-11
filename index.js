70bab8686fc81b3a5ef23dbcd4a19a53ccb140c3
Sik-Wa <sikwavivian@gmail.com>
2025-01-11 03:20:18 +0200

    json server succesfully running

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
+app.use(cors());
 
 
+//here we read the directory/where we are fetching data and synchronize it
+const {readdirSync} = require("fs")  
 
+//we map through the data in the directory(routes)
+readdirSync("./routes").map((file)=>app.use('/',require('./routes/' + file)))
+
+ //here we send the data to our index.html file where we want to display it
+ app.get('/',(req,res)=>{
+    res.sendFile(__dirname + "/index.html")
+ })
 
 
 //connecting the dot env file to this file 
